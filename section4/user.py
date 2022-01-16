4914a264fd23b9fe4869d506c5c97106af1534aa
Alex Yeo <ay@4ei.co.uk>
2022-01-16 13:05:36 +0000

    archiving/removing files

--- section4/user.py
+++
@@ -1,7 +0,0 @@
-class User:
-
-	# using _id isntead of id, as id is a python keyword
-	def __init__(self, _id, username, password):
-		self.id = _id
-		self.username = username
-		self.password = password
